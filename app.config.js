d5aca0eb301dbf4702f2915a11c5b813cde55c17
Julio Muller <juliodolago@gmail.com>
2021-02-05 22:36:57 -0300

    Configured Expo

---
+++ app.config.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv'
+import project from './package.json'
+
+const config = dotenv.config()
+
+if (config.error) {
+  throw config.error
+}
+
+export default {
+  "expo": {
+    "name": "GitLab Explorer",
+    "slug": project.name,
+    "version": project.version,
+    "orientation": "portrait",
+    "icon": "./src/images/icon.png",
+    "splash": {
+      "backgroundColor": "#2089dc",
+      "image": "./src/images/splash.png",
+      "resizeMode": "cover"
+    },
+    extra: {
+      ...process.env
+    },
+    "updates": {
+      "fallbackToCacheTimeout": 0
+    },
+    "assetBundlePatterns": [
+      "**/*"
+    ],
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./src/images/icon.png",
+        "backgroundColor": "#2089dc"
+      }
+    },
+    "ios": {
+      "supportsTablet": true
+    },
+    "web": {
+      "favicon": "./src/images/icon.png"
+    }
+  }
+}
