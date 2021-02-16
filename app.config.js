b0741832a4f82dd31a90c6b03f8f757e1556726d
Julio Muller <juliodolago@gmail.com>
2021-02-16 12:29:34 -0300

    Linted config file

--- app.config.js
+++ app.config.js
@@ -8,37 +8,37 @@ if (config.error) {
 }
 
 export default {
-  "expo": {
-    "name": "GitLab Explorer",
-    "slug": project.name,
-    "version": project.version,
-    "orientation": "portrait",
-    "icon": "./src/images/icon.png",
-    "splash": {
-      "backgroundColor": "#2089dc",
-      "image": "./src/images/splash.png",
-      "resizeMode": "cover"
+  expo: {
+    name: 'GitLab Explorer',
+    slug: project.name,
+    version: project.version,
+    orientation: 'portrait',
+    icon: './src/images/icon.png',
+    splash: {
+      backgroundColor: '#2089dc',
+      image: './src/images/splash.png',
+      resizeMode: 'cover',
     },
     extra: {
-      ...process.env
+      ...process.env,
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0
+    updates: {
+      fallbackToCacheTimeout: 0,
     },
-    "assetBundlePatterns": [
-      "**/*"
+    assetBundlePatterns: [
+      '**/*',
     ],
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./src/images/icon.png",
-        "backgroundColor": "#2089dc"
-      }
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './src/images/icon.png',
+        backgroundColor: '#2089dc',
+      },
     },
-    "ios": {
-      "supportsTablet": true
+    ios: {
+      supportsTablet: true,
     },
-    "web": {
-      "favicon": "./src/images/icon.png"
-    }
-  }
+    web: {
+      favicon: './src/images/icon.png',
+    },
+  },
 }
