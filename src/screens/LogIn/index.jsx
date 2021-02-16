79729704c7af7fd4611bd1943bd49a3183b4ca46
Julio Muller <juliodolago@gmail.com>
2021-02-16 12:33:09 -0300

    Finished "LogIn" component

--- src/screens/LogIn/index.jsx
+++ src/screens/LogIn/index.jsx
@@ -1,12 +1,81 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import constants from 'expo-constants'
+import { Image, ScrollView, Text, View } from 'react-native'
+import { Button, Input } from 'react-native-elements'
+import { RectButton } from 'react-native-gesture-handler'
+import { useAuth } from '../../hooks'
+import logo from '../../images/logo.png'
 import styles from './styles'
 
+const defaultUsername = constants.manifest.extra.GITLAB_USERNAME ?? ''
+const defaultPassword = constants.manifest.extra.GITLAB_PASSWORD ?? ''
+
 function LogIn() {
+  const { signIn } = useAuth()
+  const [message, setMessage] = useState('')
+  const [isLoading, setLoading] = useState(false)
+  const [username, setUsername] = useState(defaultUsername)
+  const [password, setPassword] = useState(defaultPassword)
+
+  async function handlePress() {
+    try {
+      setLoading(true)
+      await signIn(username, password)
+    } catch (error) {
+      setMessage(error.message)
+      setLoading(false)
+    }
+  }
+
   return (
-    <View style={styles.wrapper}>
-      <Text>Hello, LogIn!</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.screen}>
+      <Image
+        style={styles.logo}
+        source={logo}
+      />
+
+      <View style={styles.form}>
+        <Input
+          style={styles.input}
+          labelStyle={styles.label}
+          leftIconContainerStyle={styles.inputIcon}
+          leftIcon={{ name: 'person', type: 'material' }}
+          // keyboardType="email-address"
+          // autoCompleteType="email"
+          autoCapitalize="none"
+          label="Usuário do GitLab:"
+          onChangeText={setUsername}
+          value={username}
+        />
+        <Input
+          style={styles.input}
+          labelStyle={styles.label}
+          leftIconContainerStyle={styles.inputIcon}
+          leftIcon={{ name: 'vpn-key', type: 'material' }}
+          autoCompleteType="password"
+          label="Senha de acesso:"
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+        />
+
+        <If condition={message}>
+          <Text style={styles.error}>{message}</Text>
+        </If>
+
+        <View style={styles.btnWrapper}>
+          <Button
+            buttonStyle={styles.btn}
+            containerStyle={styles.btnContainer}
+            title="Entrar"
+            raised={!isLoading}
+            loading={isLoading}
+            onPress={handlePress}
+            ViewComponent={RectButton}
+          />
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
--- src/screens/LogIn/styles.jsx
+++ src/screens/LogIn/styles.jsx
@@ -1,8 +1,48 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-  wrapper: {
+  btn: {
+    backgroundColor: '#FC6D26',
+  },
+  btnContainer: {
+    textTransform: 'uppercase',
+    width: '80%',
+  },
+  btnWrapper: {
     alignItems: 'center',
+    paddingTop: 24,
+  },
+  error: {
+    color: 'red',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  form: {
+    width: '90%',
+  },
+  input: {
+    backgroundColor: 'rgba(222, 222, 222, 0.8)',
+    color: '#000',
+    fontFamily: 'Archivo400',
+  },
+  inputIcon: {
+    backgroundColor: 'rgba(222, 222, 222, 0.8)',
+    paddingHorizontal: 10,
+  },
+  label: {
+    color: '#fff',
+    fontFamily: 'Archivo400',
+    fontSize: 18,
+    marginLeft: 4,
+  },
+  logo: {
+    height: '20%',
+    marginBottom: 40,
+    resizeMode: 'contain',
+  },
+  screen: {
+    alignItems: 'center',
+    backgroundColor: '#2089dc',
     height: '100%',
     justifyContent: 'center',
   },
