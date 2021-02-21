3482f3e8cf60f44fc76c71d40650f70c23be6ce0
Julio Muller <juliodolago@gmail.com>
2021-02-20 22:43:10 -0300

    Fixed  route parameter forwarding

--- src/routes/auth/Stack/index.jsx
+++ src/routes/auth/Stack/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useRoute } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import Home from '../../../screens/Home'
 import options from './options'
 
 const { Navigator, Screen } = createStackNavigator()
 
-function Stack({ favorites }) {
+function Stack() {
+  const { favorites } = useRoute().params
+
   return (
     <Navigator screenOptions={options.navigatorScreens}>
       <Screen
